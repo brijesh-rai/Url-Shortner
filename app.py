05a672ce0f69e509c951326d42055d636b9975e4
Vishal Kumar Rai <vishalkumarrai99.com>
2026-04-05 22:53:21 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+class ActualUrl(BaseModel):
+    original_url:str
+
+class UrlData(BaseModel):
+    original_url:str
+    short_url:str
+    created_at:datetime
+    expires_at:datetime
+
+print(UrlData({
+    "original_url":"str",
+}))
+# print(UrlData("string", "nestring"))
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "Hello"
+
+@app.post("/generate_shorturl")
+def generate_shorturl(actualurl:ActualUrl):
+    print(actualurl.original_url)
+    return "IN generate short url function"
